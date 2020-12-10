b21ea48f384c81aecf966f5001694348bfabffc8
Adriel <adrielarandez@gmail.com>
2020-12-10 17:26:01 -0300

    Menu Updated

---
+++ Assets/Scripts/GameScripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject roomCanvas;
+    public GameObject creditsCanvas;
+    public GameObject menuCanvas;
+
+    public void OnClick_RoomButton()
+    {
+        menuCanvas.SetActive(false);
+        roomCanvas.SetActive(true);
+    }
+
+    public void OnClick_CreditsButton()
+    {
+        menuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void OnClick_BackButton()
+    {
+        if (creditsCanvas.activeInHierarchy)
+        {
+            creditsCanvas.SetActive(false);
+        }
+        else
+        {
+            roomCanvas.SetActive(false);
+        }
+        menuCanvas.SetActive(true);
+    }
+
+    public void OnClick_ExitButton()
+    {
+        Application.Quit();
+    }
+
+
+}
